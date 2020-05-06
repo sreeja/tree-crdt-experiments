ba060ebbecdb0068064c8e279a3fe6f24c5e85c6
Sreeja <sreeja.in@gmail.com>
2020-05-06 11:58:13 +0200

    op lisdt per replica

--- workload/crdt_gen0.py
+++ workload/crdt_gen0.py
@@ -5,26 +5,26 @@ import random
 
 
 def gen_ops(): 
-  ops = []
+  ops = [[],[],[]]
   cmd = 'http://localhost:600'
   l = ['a','b','c'] 
   for i in range(0,30): 
     for r in range(1,4): 
       parent = random.choice(l) + random.choice(l+['']) + random.choice(l+['']) 
       # print(parent, parent + str(i) + str(r)) 
-      ops += [cmd+str(r)+'/add?n='+parent + str(i) + str(r)+'&p='+parent]
+      ops[r-1] += [cmd+str(r)+'/add?n='+parent + str(i) + str(r)+'&p='+parent]
       if not i%5: 
         # print('remove this') 
-        ops += [cmd+str(r)+'/remove?n='+parent + str(i) + str(r)+'&p='+parent]
+        ops[r-1] += [cmd+str(r)+'/remove?n='+parent + str(i) + str(r)+'&p='+parent]
   n = ['a','b','c','d','e','f']
   for i in range(0,7):
     parent = random.choice(n)
     child = parent+random.choice(n)
     for r in range(1,4):
-      ops += [cmd+str(r)+'/upmove?n='+l[r-1]+child+'&p='+l[r-1]+parent+'&np='+l[r-1]]
+      ops[r-1] += [cmd+str(r)+'/upmove?n='+l[r-1]+child+'&p='+l[r-1]+parent+'&np='+l[r-1]]
     for r in range(1,4):
-      ops += [cmd+str(r)+'/downmove?n='+l[r-1]+child+'&p='+l[r-1]+'&np='+l[r-1]+parent]
+      ops[r-1] += [cmd+str(r)+'/downmove?n='+l[r-1]+child+'&p='+l[r-1]+'&np='+l[r-1]+parent]
   
   return ops
 
-# print(gen_ops())
+print(gen_ops())
