d2dcea32fc85d762436c0a8e45485d3422de8213
Sreeja <sreeja.in@gmail.com>
2020-05-22 22:41:21 +0200

    simpler script

--- experiment.sh
+++ experiment.sh
@@ -1,104 +1,33 @@
 #!/bin/bash
 > log.txt
-for LC in 1 2 3
+for LC in 1
 do
   export LC_ENV=$LC
-  t=$(expr 30 \* $LC_ENV)
-  for EXP in 0 1 2 3
+  t=$(expr 20 \* $LC_ENV)
+  for EXP in 0
     do
       export EXP_ENV=$EXP
 
-      for f in $(find data); do > $f; done
-      make down
-      make run &
-      P_PID=$!
-      sleep 60
-      sh workload/base.sh
-      sleep $t
-      sh workload/conflict0_load0.sh & sh workload/conflict0_load1.sh & sh workload/conflict0_load2.sh 
-      sleep $t
-      sh workload/conflict0_load0.sh & sh workload/conflict0_load1.sh & sh workload/conflict0_load2.sh 
-      sleep $t
-      sh workload/conflict0_load0.sh & sh workload/conflict0_load1.sh & sh workload/conflict0_load2.sh 
-      sleep $t
-      sh workload/conflict0_load0.sh & sh workload/conflict0_load1.sh & sh workload/conflict0_load2.sh 
-      sleep $t
-      sh workload/conflict0_load0.sh & sh workload/conflict0_load1.sh & sh workload/conflict0_load2.sh 
-      sleep $t
-      cp -r data/ lc$LC_ENV/data0_$EXP_ENV
-      kill $P_PID
-      sleep 45
+      for con in 0 2 10 20 
+        do
+        for f in $(find data); do > $f; done
+        make down
+        make run &
+        P_PID=$!
+        sleep 60
+        sh workload/base.sh
+        sleep $t
+        for i in `seq 0 4`; do
+          sh workload/conflict$con.sh
+        done
+        sleep $t
+        cp -r data/ lc$LC_ENV/data$con/$EXP_ENV
+        kill $P_PID
+        sleep 45
 
-      echo "Done lc" $LC_ENV " exp "$EXP_ENV " con " 0 >> log.txt
+        echo "Done lc" $LC_ENV " exp "$EXP_ENV " con " $con >> log.txt
+      done
 
-      for f in $(find data); do > $f; done
-      make down
-      make run &
-      P_PID=$!
-      sleep 60
-      sh workload/base.sh
-      sleep $t
-      sh workload/conflict2_load0.sh & sh workload/conflict2_load1.sh & sh workload/conflict2_load2.sh 
-      sleep $t
-      sh workload/conflict2_load0.sh & sh workload/conflict2_load1.sh & sh workload/conflict2_load2.sh 
-      sleep $t
-      sh workload/conflict2_load0.sh & sh workload/conflict2_load1.sh & sh workload/conflict2_load2.sh 
-      sleep $t
-      sh workload/conflict2_load0.sh & sh workload/conflict2_load1.sh & sh workload/conflict2_load2.sh 
-      sleep $t
-      sh workload/conflict2_load0.sh & sh workload/conflict2_load1.sh & sh workload/conflict2_load2.sh 
-      sleep $t
-      cp -r data/ lc$LC_ENV/data2_$EXP_ENV
-      kill $P_PID
-      sleep 45
-
-      echo "Done lc" $LC_ENV " exp "$EXP_ENV " con " 2 >> log.txt
-
-      for f in $(find data); do > $f; done
-      make down
-      make run &
-      P_PID=$!
-      sleep 60
-      sh workload/base.sh
-      sleep $t
-      sh workload/conflict10_load0.sh & sh workload/conflict10_load1.sh & sh workload/conflict10_load2.sh 
-      sleep $t
-      sh workload/conflict10_load0.sh & sh workload/conflict10_load1.sh & sh workload/conflict10_load2.sh 
-      sleep $t
-      sh workload/conflict10_load0.sh & sh workload/conflict10_load1.sh & sh workload/conflict10_load2.sh 
-      sleep $t
-      sh workload/conflict10_load0.sh & sh workload/conflict10_load1.sh & sh workload/conflict10_load2.sh 
-      sleep $t
-      sh workload/conflict10_load0.sh & sh workload/conflict10_load1.sh & sh workload/conflict10_load2.sh 
-      sleep $t
-      cp -r data/ lc$LC_ENV/data10_$EXP_ENV
-      kill $P_PID
-      sleep 45
-
-      echo "Done lc" $LC_ENV " exp "$EXP_ENV " con " 10 >> log.txt
-
-      for f in $(find data); do > $f; done
-      make down
-      make run &
-      P_PID=$!
-      sleep 60
-      sh workload/base.sh
-      sleep $t
-      sh workload/conflict20_load0.sh & sh workload/conflict20_load1.sh & sh workload/conflict20_load2.sh 
-      sleep $t
-      sh workload/conflict20_load0.sh & sh workload/conflict20_load1.sh & sh workload/conflict20_load2.sh 
-      sleep $t
-      sh workload/conflict20_load0.sh & sh workload/conflict20_load1.sh & sh workload/conflict20_load2.sh 
-      sleep $t
-      sh workload/conflict20_load0.sh & sh workload/conflict20_load1.sh & sh workload/conflict20_load2.sh 
-      sleep $t
-      sh workload/conflict20_load0.sh & sh workload/conflict20_load1.sh & sh workload/conflict20_load2.sh 
-      sleep $t
-      cp -r data/ lc$LC_ENV/data20_$EXP_ENV
-      kill $P_PID
-      sleep 45
-
-      echo "Done lc" $LC_ENV " exp "$EXP_ENV " con " 20 >> log.txt
     done
 done
 
