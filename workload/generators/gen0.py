92f33febb310e7304954333e8fb843ee6f5fd757
Sreeja <sreeja.in@gmail.com>
2020-05-29 11:41:37 +0200

    zero conflict load

--- workload/generators/gen0.py
+++ workload/generators/gen0.py
@@ -1,6 +1,6 @@
 '''
-30 adds, mixed with 6 removes and then 14 moves
-critical ancestors to be given manually now
+150 adds, mixed with 30 removes and then 70 moves for 3 replicas
+250 ops per replica = 750 ops together
 '''
 import random
 
@@ -8,23 +8,24 @@ import random
 def gen_ops(): 
   ops = [[],[],[]]
   cmd = 'http://localhost:600'
-  l = ['a','b','c'] 
-  for i in range(0,30): 
+  l = ['a','b','c','d','e','f','g'] 
+  for i in range(0,150): 
     for r in range(1,4): 
-      parent = random.choice(l) + random.choice(l+['']) + random.choice(l+['']) 
+      parent = l[r-1] + random.choice(l+['']) + random.choice(l+['']) 
       # print(parent, parent + str(i) + str(r)) 
       ops[r-1] += [cmd+str(r)+'/add?n='+parent + str(i) + str(r)+'&p='+parent]
       if not i%5: 
         # print('remove this') 
         ops[r-1] += [cmd+str(r)+'/remove?n='+parent + str(i) + str(r)+'&p='+parent]
   n = ['a','b','c','d','e','f','g']
-  for i in range(0,7):
-    parent = random.choice(n)
-    child = parent+random.choice(n)
+  for i in range(0,35):
+    parent = random.choice(l)
+    child = parent+random.choice(l)
     for r in range(1,4):
-      ops[r-1] += [cmd+str(r)+'/upmove?n='+l[r-1]+child+'&p='+l[r-1]+parent+'&np='+l[r-1]]
-      ops[r-1] += [cmd+str(r)+'/downmove?n='+l[r-1]+child+'&p='+l[r-1]+'&np='+l[r-1]+parent]
-  
+      # up
+      ops[r-1] += [cmd+str(r)+'/move?n='+l[r-1]+child+'&p='+l[r-1]+parent+'&np='+l[r-1]]
+      # down
+      ops[r-1] += [cmd+str(r)+'/move?n='+l[r-1]+child+'&p='+l[r-1]+'&np='+l[r-1]+parent]
   return ops
 
 # print(gen_ops())
