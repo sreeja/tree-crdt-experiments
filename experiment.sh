d2ee7724610b78bfcc99febb0e72e909d9c844ed
Sreeja <sreeja.in@gmail.com>
2020-07-22 15:35:49 +0200

    edit

--- experiment.sh
+++ experiment.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 > log.txt
-for run in $(seq 5)
+for LC in 3 2 1
 do
-  for LC in 3 2 1
+  export LC_ENV=$LC
+  t=$(expr 20 \* $LC_ENV)
+  for EXP in 0 1 2 3
   do
-    export LC_ENV=$LC
-    t=$(expr 20 \* $LC_ENV)
-    for EXP in 0 1 2 3
+    export EXP_ENV=$EXP
+    for con in 0 2 10 20
+    do
+      for run in $(seq 15)
       do
-        export EXP_ENV=$EXP
+        for f in $(find data); do > $f; done
+        make down
+        make run &
+        P_PID=$!
+        sleep 60
+        sh workload/base.sh
+        sleep $t
+        sh workload/conflict$con.sh
+        sleep $t
+        mkdir -p run$run/lc$LC_ENV/data$con/$EXP_ENV
+        cp -r data/ run$run/lc$LC_ENV/data$con/$EXP_ENV
+        kill $P_PID
+        sleep 45
 
-        for con in 0 2 10 20
-          do
-            for f in $(find data); do > $f; done
-            make down
-            make run &
-            P_PID=$!
-            sleep 60
-            sh workload/base.sh
-            sleep $t
-            sh workload/conflict$con.sh
-            sleep $t
-            mkdir -p run$run/lc$LC_ENV/data$con/$EXP_ENV
-            cp -r data/ run$run/lc$LC_ENV/data$con/$EXP_ENV
-            kill $P_PID
-            sleep 45
-
-            echo "Done run" $run "lc" $LC_ENV " exp "$EXP_ENV " con " $con >> log.txt
-          done
-
+        echo "Done run" $run "lc" $LC_ENV " exp "$EXP_ENV " con " $con >> log.txt
       done
+    done
   done
-
 done
 
 for f in $(find data); do > $f; done
